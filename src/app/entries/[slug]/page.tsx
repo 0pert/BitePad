ab887b84321f4edcbb3698e9ca04678cd78e7a5c
Oliver Persson <oliverpersson_580@hotmail.com>
2026-03-25 20:22:54 +0100

    edit img fix

--- src/app/entries/[slug]/page.tsx
+++ src/app/entries/[slug]/page.tsx
@@ -11,27 +11,35 @@ type Props = {
 
 export default async function EntryPage({ params }: Props) {
   const { slug } = await params;
-  console.log(slug)
+  console.log(slug);
   const entry = await loadEntryBySlug(slug);
 
   if (!entry) notFound();
-  console.log(entry)
+  // console.log(entry)
 
   return (
-    <main style={{ padding: 12, width: "100%", maxWidth: 900, margin: "0 auto" }}>
-      <div style={{ display: "flex", alignContent:"space-between", alignItems: "center" }}>
+    <main
+      style={{ padding: 12, width: "100%", maxWidth: 900, margin: "0 auto" }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <div>
-        <Link href="/">
-          <img src="../../left-arrow.png" className="left-arrow" />
-        </Link><br/>
-        <Link href={`/entries/edit/${entry.slug}`}>
-                <img src="edit.png" className="edit-icon" />
-                </Link>
+          <Link href="/">
+            <img src="../../left-arrow.png" className="left-arrow" />
+          </Link>
+          <br />
+          <Link href={`/entries/edit/${entry.slug}`}>
+            <img src="../../edit.png" className="edit-icon" />
+          </Link>
         </div>
         <div>
-
           <h1 style={{ fontSize: 30 }}>{entry.title}</h1>
-        
+
           <p>
             {entry.type} · {entry.status}
             {entry.timeMinutes ? ` · ${entry.timeMinutes} min` : ""}
